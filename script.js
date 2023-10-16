a9979f1da54749d075661bc12ad45a59f2ad7141
Zachary Whiteley <zachary.whiteley@sky.uk>
2023-10-16 12:05:59 +0100

    Ensure lightbulb exclusivity.
    Ensure only one lightbulb is active at any one time.

--- script.js
+++ script.js
@@ -2,12 +2,32 @@ let count = 0;
 
 const lightbulb_message = document.querySelector(".subtitle");
 
-for (const lightbulb of document.getElementsByClassName("lightbulb")) {
-    lightbulb.addEventListener("click", () => {
-        lightbulb.classList.toggle("active");
-        lightbulb_message.textContent = `You've clicked the lights ${++count}`;
+function lightbulb_handler(ev) {
+    // Turn off all other lights
+    const active_lightbulbs = document.querySelectorAll(".lightbulb.active");
 
-        if (count === 1) lightbulb_message.textContent += " time";
-        else lightbulb_message.textContent += " times";
-    });
+    if (active_lightbulbs.length !== 1) {
+        alert("An error occurred");
+        return;
+    }
+
+    const lightbulb = ev.target;
+    const active_lightbulb = active_lightbulbs[0];
+
+    // Don't do anything if clicking on an active lightbulb
+    if (active_lightbulb === lightbulb) return;
+
+
+    // Activate the target
+    active_lightbulb.classList.remove("active");
+    lightbulb.classList.add("active");
+    lightbulb_message.textContent = `You've clicked the lights ${++count}`;
+
+    if (count === 1) lightbulb_message.textContent += " time";
+    else lightbulb_message.textContent += " times";
+
+}
+
+for (const lightbulb of document.getElementsByClassName("lightbulb")) {
+    lightbulb.addEventListener("click", lightbulb_handler);
 }
