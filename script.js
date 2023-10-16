32a768dee57eaf752bd9a88b0baf1e955be33060
Zachary Whiteley <zachary.whiteley@sky.uk>
2023-10-16 12:01:43 +0100

    Add basic toggle functionality.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Write your code here
+let count = 0;
+
+const lightbulb_message = document.querySelector(".subtitle");
+
+for (const lightbulb of document.getElementsByClassName("lightbulb")) {
+    lightbulb.addEventListener("click", () => {
+        lightbulb.classList.toggle("active");
+        lightbulb_message.textContent = `You've clicked the lights ${++count}`;
+
+        if (count === 1) lightbulb_message.textContent += " time";
+        else lightbulb_message.textContent += " times";
+    });
+}
